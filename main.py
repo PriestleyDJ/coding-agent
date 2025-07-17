6662b31b9dad61ab0b5ef744a510276851b33e0a
priestleydj <priestleydj@gmail.com>
2025-07-17 01:10:49 +0100

    Add basic ai functionality

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from coding-agent!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+# Load environment
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+# Check for arguments
+parser = argparse.ArgumentParser(description="AI Coding Assistant")
+parser.add_argument("user_prompt", help="Your prompt for the AI Coding Assistant")
+parser.add_argument(
+    "-v", "--verbose", help="A more verbose output", action="store_true"
+)
+args = parser.parse_args()
+user_prompt = args.user_prompt
 
 
-if __name__ == "__main__":
-    main()
+# Message history
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+# Generate response
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+)
+
+if args.verbose:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"User prompt: {user_prompt}")
+
+print(f"Response: {response.text}")
