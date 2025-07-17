b676f6cc40ae663c60a88aa0ba821159c48e8f7d
priestleydj <priestleydj@gmail.com>
2025-07-17 19:46:20 +0100

    Allow agent to call available functions

--- call_functions.py
+++ call_functions.py
@@ -1,8 +1,9 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
+from config import WORKING_DIRECTORY
 
 # Get available functions
 available_functions = types.Tool(
@@ -13,3 +14,42 @@ available_functions = types.Tool(
         schema_write_file,
     ]
 )
+
+function_map = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+}
+
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    function_args = dict(function_call_part.args)
+    function_args["working_directory":WORKING_DIRECTORY]
+
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    function_result = function_map[function_name](**function_args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name, response={"result": function_result}
+            )
+        ],
+    )
--- main.py
+++ main.py
@@ -3,44 +3,63 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from call_functions import available_functions
+from call_functions import available_functions, call_function
 from prompts import system_prompt
 
-# Load environment
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main()
+    # Load environment
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-# Check for arguments
-parser = argparse.ArgumentParser(description="AI Coding Assistant")
-parser.add_argument("user_prompt", help="Your prompt for the AI Coding Assistant")
-parser.add_argument(
-    "-v", "--verbose", help="A more verbose output", action="store_true"
-)
-args = parser.parse_args()
-user_prompt = args.user_prompt
+    # Check for arguments
+    parser = argparse.ArgumentParser(description="AI Coding Assistant")
+    parser.add_argument("user_prompt", help="Your prompt for the AI Coding Assistant")
+    parser.add_argument(
+        "-v", "--verbose", help="A more verbose output", action="store_true"
+    )
+    args = parser.parse_args()
+    user_prompt = args.user_prompt
 
-# Message history
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
+    # Message history
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
-# Generate response
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-    config=types.GenerateContentConfig(
-        tools=[available_functions], system_instruction=system_prompt
-    ),
-)
+    generate_content(client, messages, args.verbose)
 
-if args.verbose:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"User prompt: {user_prompt}")
+def generate_content(client, messages, verbose):
+    # Generate response
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
 
-if response.function_calls:
-    for function in response.function_calls:
-        print(f"Calling function: {function.name}({function.args})")
-else:
-    print(f"Response: {response.text}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"User prompt: {user_prompt}")
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+
+if __name__ == "__main__":
+    main()
